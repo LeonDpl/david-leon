7920fdecc0527c8ee906783b85ec315205fe4efb
David Léon <david.leon@predict.fr>
2021-06-30 12:21:09 +0200

    upd

---
+++ README.md
@@ -0,0 +1,5 @@
+## Links
+  - https://towardsdatascience.com/a-quick-tutorial-on-how-to-deploy-your-streamlit-app-to-heroku-874e1250dadd
+  - https://www.heroku.com/platform
+  - https://www.innerdoc.com/nlp-timeline-demo/
+  - https://timeline.knightlab.com/docs/json-format.html
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 # Streamlit Timeline Component Example
-
+import datetime as dt
 import streamlit as st
 from streamlit_timeline import timeline
+
+_now = dt.datetime.now()
+_now_str = '{"year": "'+str(_now.year)+'","month": "'+str(_now.month)+'","display_date": "now"}'
 
 
 # use full page width
@@ -10,6 +13,8 @@ st.set_page_config(page_title="David Leon - Personnal Experience", layout="wide"
 # load data
 with open('data.json', "r", encoding="latin-1") as f:
     data = f.read()
+
+data = data.replace('"#NOW#"',_now_str)
 
 # render timeline
 timeline(data, height=800)
